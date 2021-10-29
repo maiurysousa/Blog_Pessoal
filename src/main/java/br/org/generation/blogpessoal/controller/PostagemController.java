407606e198a8917c24023ef9e389c8d493955fb3
maiurysousa <maiuryprogramando@gmail.com>
2021-10-29 15:28:15 -0300

    adicionando CRUD

--- src/main/java/br/org/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/br/org/generation/blogpessoal/controller/PostagemController.java
@@ -3,9 +3,15 @@ package br.org.generation.blogpessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +29,36 @@ public class PostagemController {
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){
 		return ResponseEntity.ok(postagemRepository.findAll());
-		// select * from tb_postagens;		
+		// select * from tb_postagens;
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable long id){
+		return postagemRepository.findById(id) //método de busca
+				.map(resposta -> ResponseEntity.ok(resposta)) // mostra e dá um status de ok//ou vai receber o dado ou um objto nulo
+				.orElse(ResponseEntity.notFound().build());
+		// select * from tb_postagens where id = 1;
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity <List <Postagem>> getById(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo)); //método de busca
+	
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> postPostagem(@RequestBody Postagem postagem){ //parâmetro do tipo postagem que vou chamar depostagem
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> putPostagem(@RequestBody Postagem postagem){ //parâmetro do tipo postagem que vou chamar depostagem
+		return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletePostagem (@PathVariable long id) {
+		postagemRepository.deleteById(id);
+	}
+	
 }
